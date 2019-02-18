b199da4db3253b19e153e4cc3ca682f2912635fd
Anurag Roy <anuragroy17@gmail.com>
2019-02-18 17:10:18 +0530

    First Commit

---
+++ src/sample/Main.java
@@ -0,0 +1,36 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage){
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+            primaryStage.setTitle("Calculator");
+            primaryStage.getIcons().add(new Image("Images/icon.png"));
+            primaryStage.setScene(new Scene(root, 400, 460));
+            primaryStage.getScene().getStylesheets().addAll(getClass().getResource("style.css").toExternalForm());
+            root.requestFocus();
+            primaryStage.show();
+            primaryStage.setResizable(false);
+            primaryStage.sizeToScene();
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
